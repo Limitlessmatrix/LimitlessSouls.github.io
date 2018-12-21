c614f2e40352988e35381e07241cbe61979f06ca
Jeremy Retamar Arrieta <jakdtak@gmail.com>
2018-12-20 23:09:20 -0700

    code separated by files
    New commit, updating code to be in its own file. 
    Added to do items.
    Add active button with event listeners.

---
+++ app.js
@@ -0,0 +1,19 @@
+let content = document.getElementById("content")
+let button = document.getElementById("show-more")
+
+button.onclick = function(){
+
+    if(content.className=="open") {
+ //shrink
+        content.className="";
+        button.innerHTML= "Show More";
+
+    } else{
+        //expand
+        content.className=="open";
+        button.innerHTML = "Show Less";
+    }
+
+   
+
+}
