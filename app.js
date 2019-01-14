266f19e690309030e783a1c68986ef6af1eb9095
Jeremy Retamar Arrieta <jakdtak@gmail.com>
2019-01-14 15:44:30 -0700

    Less complicated button

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 document.getElementById("btn").addEventListener("click" , function (){ alert('Praise the Sun!'); });
 
 
-const user_number = (e) => {
-        e.preventDefault();
-        const value = parseInt(document.getElementById('takeinput').value, 10);
-        const message = (9000 >= value) ? 'Not many souls' : 'Thats over 9,000!';
-        console.log(message);
-      }
-      
-      const button = document.getElementById('button');
-      button.addEventListener('click', user_number);
+let button = document.getElementById("button")
+
+let user_number=  function(){
+  var input = document.getElementById("takeinput").value
+  if (input<=9000) console.log("Not many souls")
+  else console.log("That's over 9,000!")
+}
+
+button.addEventListener("click", user_number)
 
 
 
@@ -20,23 +20,3 @@ for (i = 1; i<30; i++)
         console.log(i)
 
 
-/*let button = document.getElementById("button")
-
-var input = document.getElementById("number_of_souls").getElementById("takeinput").value
-
-let user_number=  function(){
-    
-    for (let i = 1; i<1; i++) {
-
-        if (9000<input) {
-
-        alert("Not many souls") }
-
-        else 9000>input 
-
-        alert("That's over 9,000!")
-}
-}
-
-button.addEventListener("click", user_number)
-   */
