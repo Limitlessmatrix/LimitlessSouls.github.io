9d1cde5dae63894671ed404c549304c224c1ed46
Jeremy Retamar Arrieta <jakdtak@gmail.com>
2019-01-15 08:44:43 -0700

    Added form functionality
    Changed from only clicking submit button to accepting the enter key for the question "How many souls have you acquired?

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 document.getElementById("btn").addEventListener("click" , function (){ alert('Praise the Sun!'); });
 
 
-let button = document.getElementById("button")
+let form = document.getElementById("number_of_souls")
 
-let user_number=  function(){
+let user_number=  function(e){
+  e.preventDefault()
   var input = document.getElementById("takeinput").value
-  if (input<=9000) console.log("Not many souls")
-  else console.log("That's over 9,000!")
+  if (input<=9000) 
+        {console.log("Not many souls")
+      }
+  else  {console.log("That's over 9,000!")
+      }
 }
 
-button.addEventListener("click", user_number)
+form.addEventListener("submit", user_number)
 
 
 
-for (i = 1; i<30; i++)
-        if (i > 11) 
-        console.log(i+ " PASSED 11")
-        else
+for (i = 1; i<30; i++) {
+        if (i > 11) {
+        console.log( i + " PASSED 11")
+    }   else{
         console.log(i)
+    }
+}
 
 
